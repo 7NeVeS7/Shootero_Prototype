a4367121f0bec2246cf934006b1b1f34f1200924
jahoo99 <62152889+jahoo99@users.noreply.github.com>
2020-03-14 19:35:08 +0100

    Movement

---
+++ Shootero_Prototype/Assets/Scripts/MovementCharacter.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementCharacter : MonoBehaviour
+{
+    [SerializeField]
+    private float moveSpeed = 5f;
+    [SerializeField]
+    private Rigidbody rb;
+    Vector3 movement;
+    private void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.z = Input.GetAxisRaw("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}
