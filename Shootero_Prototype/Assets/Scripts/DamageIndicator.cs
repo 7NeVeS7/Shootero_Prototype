4b27524014e39e72be67aa5ea6d0269dbee882e5
jahoo99 <62152889+jahoo99@users.noreply.github.com>
2020-03-15 17:19:17 +0100

    Obrażenia i umieranie
    Gracz otrzymuje obrażenia i umiera

---
+++ Shootero_Prototype/Assets/Scripts/DamageIndicator.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageIndicator : MonoBehaviour
+{
+    [SerializeField]
+    private  HealthPoints scoreManager;
+    public string dead;
+    private void OnTriggerEnter(Collider other)
+    {
+        //Śmierć
+        if (other.gameObject.tag == "Player")
+        {
+            dead = "very";
+            LoseHP();
+        }
+    }
+    private void LoseHP()
+    {
+        scoreManager.LoseHP(25);
+    }
+}
---
+++ Shootero_Prototype/Assets/Scripts/HealthPoints.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class HealthPoints : MonoBehaviour
+{
+    [SerializeField]
+    private int maxHP = 100;
+    [SerializeField]
+    private int CurrentHP;
+    
+    private void Start()
+    {
+        CurrentHP = maxHP; 
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        //Śmierć
+        if (other.gameObject.tag == "Enemy")
+        {
+            LoseHP(20);
+            Check();
+        }
+    }
+    public void LoseHP(int amount)
+    {
+        CurrentHP -= amount;
+        Check();
+    }
+    void Check()
+    {
+        if (CurrentHP>maxHP)
+        {
+            CurrentHP = maxHP;
+        }else if (CurrentHP<=0)
+        {
+            Dead();
+        }
+    }
+    void Dead()
+    {
+        SceneManager.LoadScene("SampleScene", LoadSceneMode.Single);
+    }
+}
