5d9cd20c76c177aba3564d6ec306a9c1badae382
jahoo99 <62152889+jahoo99@users.noreply.github.com>
2020-03-17 11:44:48 +0100

    konekting

---
+++ Shootero_Prototype/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    public Transform player;
+    [SerializeField]
+    private float speed = 0.1f;
+    void Update()
+    {
+        if (Vector3.Distance(player.position, this.transform.position) < 20)
+        {
+            Vector3 direction = player.position - this.transform.position;
+            direction.y = 0;
+            this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.LookRotation(direction), 0.1f);
+            if (direction.magnitude > 1)
+            {
+                this.transform.Translate(0, 0, speed);
+            }
+        }
+    }
+}
