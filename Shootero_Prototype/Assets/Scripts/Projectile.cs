69358054c62576c4185a939c030590b4564eec04
jahoo99 <62152889+jahoo99@users.noreply.github.com>
2020-03-19 16:20:54 +0100

    Strzelanie
    Postać strzela przed siebiepo kliknięciu LPM.  pociski nie naprowadzają się na przeciwnika ani nie są wystrzeliwane po zatrzymaniu się.

---
+++ Shootero_Prototype/Assets/Scripts/Projectile.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile: MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 20f;
+    public Rigidbody rb;
+
+    private void Start()
+    {
+        rb.velocity = transform.forward * speed;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (other.gameObject.tag == "Enemy")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
